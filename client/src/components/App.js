6baf6bec33934ee9aed6c8c3baa26708c3d110cc
Benny Sufrin <bennysuf@gmail.com>
2023-03-28 14:47:26 -0400

    adds routes and Signup component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Route, Switch, useHistory, Link } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
+import Signup from "./Signup";
 function App() {
   useEffect(() => {
     // fetch("/users")
@@ -15,8 +16,13 @@ function App() {
         <Route exact path="/login">
           <Login />
         </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
       </Switch>
-      <Home />
     </>
   );
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,50 +1,71 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Login(){
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordConfirm, setPasswordConfirm] = useState("")
-    
-    function handleUsername(e){
-        e.preventDefault()
-        setUsername(e.target.value)
-    }
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
-    function handlePassword(e){
-        e.preventDefault()
-        setPassword(e.target.value)
-    }
+  function handleUsername(e) {
+    e.preventDefault();
+    setUsername(e.target.value);
+  }
 
-    function handlePasswordConfirm(e){
-        e.preventDefault()
-        setPasswordConfirm(e.target.value)
-    }
+  function handlePassword(e) {
+    e.preventDefault();
+    setPassword(e.target.value);
+  }
 
-    function handleLogin() {
-        
-        fetch("/login", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    username: username,
-                    password: password,
-                    password_confirmation: passwordConfirm
-                })
-        })
-        .then(r => r.json())
-        .then(d => console.log(d))
-    }
+  function handlePasswordConfirm(e) {
+    e.preventDefault();
+    setPasswordConfirm(e.target.value);
+  }
 
-    
-    return (
-        <div>
-            <input type="text" id="username" placeholder="Username" value={username} onChange={handleUsername}/>
-            <br/>
-            <input type="password" id="password" placeholder="Password" value={password} onChange={handlePassword}/>
-            <br/>
-            <input type="password" id="passwordConfirm" placeholder="Confirm Password" value={passwordConfirm} onChange={handlePasswordConfirm}/>
-            <br/>
-            <button type="button" onClick={handleLogin}>Login</button>
-        </div>
-    );
+  function handleLogin() {
+    fetch("/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        password_confirmation: passwordConfirm,
+      }),
+    })
+      .then((r) => r.json())
+      .then((d) => console.log(d));
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        id="username"
+        placeholder="Username"
+        value={username}
+        onChange={handleUsername}
+      />
+      <br />
+      <input
+        type="password"
+        id="password"
+        placeholder="Password"
+        value={password}
+        onChange={handlePassword}
+      />
+      <br />
+      <input
+        type="password"
+        id="passwordConfirm"
+        placeholder="Confirm Password"
+        value={passwordConfirm}
+        onChange={handlePasswordConfirm}
+      />
+      <br />
+      <button type="button" onClick={handleLogin}>
+        Login
+      </button>
+      <br/>
+      <Link to="/signup">{"Signup"}</Link>
+    </div>
+  );
 }
