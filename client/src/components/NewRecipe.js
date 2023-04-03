f7dec34937fd4850ae4824935e2caa8bdd9f7a20
Benny Sufrin <bennysuf@gmail.com>
2023-04-03 13:24:42 -0400

    adds component skeliton

---
+++ client/src/components/NewRecipe.js
@@ -0,0 +1,18 @@
+
+export default function NewRecipe(){
+
+
+    function handleEdit() {
+
+    }
+
+    return (
+        <>
+        <form onSubmit={handleEdit}>
+            {/* text field here */}
+            <button type="submit">Edit</button>
+        </form>
+        </>
+    )
+
+}
