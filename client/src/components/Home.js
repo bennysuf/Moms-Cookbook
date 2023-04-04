f3428960740de8dcb2e40baa8a071ed0fa90819b
Benny Sufrin <bennysuf@gmail.com>
2023-04-04 11:42:59 -0700

    removes comments, adds recipe state, adds recipe map

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,26 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Logout from "./Logout";
 import NewRecipe from "./NewRecipe";
 import RecipeCard from "./RecipeCard";
 
+export default function Home({ user }) {
+  const [recipe, setRecipe] = useState(user.recipes);
 
-export default function Home({ recipe }) {
-    // const [recipe, setRecipe] = useState(user.recipes) //*when page reloads, we lose definition for a moment, breaking code
-//   const { username, recipes } = user; //?use state for recipes? need use state to update 
+  const recipes = recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec} />);
 
-
-const recipes = recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec}/>)
-
-  function handleAdd(){
+  function handleAdd() {
     //TODO: take me to new recipe route
   }
-
-  //console.log("!user", user === null) //*works, gets true and false 
-       //can do this in App then have the recipecard component do the work home was doing (Logout..)
-
-
-
-    // const recipes = user.recipes.map((rec)=> {if(!rec) return <RecipeCard key={rec.id} rec={rec}/>})
 
   return (
     <>
@@ -28,13 +18,9 @@ const recipes = recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec}/>)
       <button onClick={handleAdd}>new recipe</button>
       {/* <NewRecipe/> */}
       <h1>Home </h1>
-        {recipes}
-     {/* {user !== null ? <RecipeCard key={user.id} recipe={user.recipes}/> : null} */}
+      <ul>
+      {recipes}
+      </ul>
     </>
   );
 }
-//? use context? map in recipcard, call component here 
-//need to use ternary because if recipes == undefined i get error
-//! user returns undefined first, causing error.
-//* how to prevent undefined?
-//* ^ when reloaded, parent has user === []. causing undefined in child
