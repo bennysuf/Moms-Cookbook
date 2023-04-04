43e1702a104bad1905c942191d36da0735f5e0da
Benny Sufrin <bennysuf@gmail.com>
2023-04-04 11:29:25 -0700

    removes comments, adds map for recipes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,27 +1,22 @@
 import React, {useState} from "react";
 import Logout from "./Logout";
-// import NewRecipe from "./NewRecipe";
+import NewRecipe from "./NewRecipe";
 import RecipeCard from "./RecipeCard";
 
 
-export default function Home({ user }) {
-    // console.log("Home", user)
+export default function Home({ recipe }) {
     // const [recipe, setRecipe] = useState(user.recipes) //*when page reloads, we lose definition for a moment, breaking code
-    // console.log("from home", user.recipes);
 //   const { username, recipes } = user; //?use state for recipes? need use state to update 
 
-//   console.log(recipe)
+
+const recipes = recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec}/>)
 
   function handleAdd(){
     //TODO: take me to new recipe route
   }
 
   //console.log("!user", user === null) //*works, gets true and false 
-// if (user !== null) return (
-//     // <Home>
-//         <RecipeCard key={user.id} recipe={user.recipes}/> //? how to have home and this displayed at the same time? 
-//    // {/* </Home>  */}
-//        )//can do this in App then have the recipecard component do the work home was doing (Logout..)
+       //can do this in App then have the recipecard component do the work home was doing (Logout..)
 
 
 
@@ -33,10 +28,8 @@ export default function Home({ user }) {
       <button onClick={handleAdd}>new recipe</button>
       {/* <NewRecipe/> */}
       <h1>Home </h1>
-     {user !== null ? <RecipeCard key={user.id} recipe={user.recipes}/> : null}
-      {/* <ul> */}
-        {/* {!recipe ? recipe.map((rec) => <RecipeCard key={rec.id} rec={rec} setRecipe={setRecipe}/>) : console.log("made it here")} */}
-      {/* </ul> */}
+        {recipes}
+     {/* {user !== null ? <RecipeCard key={user.id} recipe={user.recipes}/> : null} */}
     </>
   );
 }
