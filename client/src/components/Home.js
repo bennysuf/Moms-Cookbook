807200122141bec4e308269ad6d423f9689efc3e
Benny Sufrin <bennysuf@gmail.com>
2023-04-04 13:11:48 -0700

    creates nested route

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import {Route, useRouteMatch, useParams} from "react-router-dom"
 import Logout from "./Logout";
-import NewRecipe from "./NewRecipe";
+// import NewRecipe from "./NewRecipe";
 import RecipeCard from "./RecipeCard";
+import RecipeEdit from "./RecipeEdit";
+// import RecipeEdit from "./RecipeEdit";
 
 export default function Home({ user }) {
   const [recipe, setRecipe] = useState(user.recipes);
+
+//   const {recipeId} = useParams();
+//   const {path } = useRouteMatch();
+
+
+//   console.log(path)
 
   const recipes = recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec} />);
 
@@ -12,15 +21,20 @@ export default function Home({ user }) {
     //TODO: take me to new recipe route
   }
 
+
   return (
-    <>
+    <div onClick={() => console.log("clicked")}>
       <Logout />
       <button onClick={handleAdd}>new recipe</button>
       {/* <NewRecipe/> */}
       <h1>Home </h1>
       <ul>
+      {/* <RecipeCard recipe={recipe} /> */}
+      <Route path={`/home/:recipeId`}>
+        <RecipeEdit recipe={recipe}/>
+      </Route>
       {recipes}
       </ul>
-    </>
+    </div>
   );
 }
