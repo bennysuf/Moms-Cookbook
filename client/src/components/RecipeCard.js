c47094673d492bd74d822312d1347fabd4b27167
Benny Sufrin <bennysuf@gmail.com>
2023-04-04 11:44:28 -0700

    cleans up page

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,35 +1,17 @@
 export default function RecipeCard({ recipe }) {
-  // export default function RecipeCard({rec, setRecipe}){
-//   console.log("recipes", recipe);
-//   const { title, directions, ingredients } = recipe;
 
-  const recipes = recipe.map((rec) => {
-    return (
-      <li key={rec.id}>
-        <h3>{rec.title}</h3>
-        <p>{rec.directions}</p>
-        <p>{rec.ingredients}</p>
-        <button onClick={onEdit}>Edit</button>
-      </li>
-    );
-  });
+  const { title, directions, ingredients } = recipe;
 
   function onEdit() {
     //take me to nested route
   }
 
   return (
-        
-    <div>
-         {recipes}
-    </div>
-    // <ul>
-    // </ul>
-    // <li>
-    //     <h3>{title}</h3>
-    //     <p>{directions}</p>
-    //     <p>{ingredients}</p>
-    //     <button onClick={onEdit}>Edit</button>
-    // </li>
+    <li>
+        <h3>{title}</h3>
+        <p>{directions}</p>
+        <p>{ingredients}</p>
+        <button onClick={onEdit}>Edit</button>
+    </li>
   );
 }
