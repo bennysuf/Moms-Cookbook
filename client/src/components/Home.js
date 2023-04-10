728e6cab0da7337a8704906106d071774f2b47e8
Benny Sufrin <bennysuf@gmail.com>
2023-04-09 17:47:03 -0700

    if no user, pushes user to login. changes return

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-import { Route, useRouteMatch, useParams } from "react-router-dom";
+import { Route, Link, useHistory } from "react-router-dom";
 import Logout from "./Logout";
 // import NewRecipe from "./NewRecipe";
 import RecipeCard from "./RecipeCard";
 import RecipeEdit from "./RecipeEdit";
 
-export default function Home({ user }) {
+export default function Home({ user, setUser }) {
+  const history = useHistory();
+
+  if (!user) {
+    history.push("/login");
+  } //need this here, wont work in App component (will always push to login before user gets updated)
+
   const [recipe, setRecipe] = useState(user.recipes);
 
   const recipes = recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec} />);
@@ -15,18 +21,25 @@ export default function Home({ user }) {
   }
 
   return (
-    <div>
-      {/* <div onClick={() => console.log("clicked")}> */}
-      <Logout />
-      <button onClick={handleAdd}>new recipe</button>
-      {/* <NewRecipe/> */}
-      <h1>Home </h1>
-      <ul>
-        <Route path={`/home/:recipeId`}>
-          <RecipeEdit recipe={recipe} setRecipe={setRecipe} />
-        </Route>
-        {recipes}
-      </ul>
-    </div>
+    <>
+      {/* <Route exact path="/add-new">
+        <NewRecipe recipe={recipe} setRecipe={setRecipe} />
+      </Route>  */}
+      {/* <Route path="home"> */}
+        <div>
+          {/* //   <div onClick={() => history.push("/home")}> */}
+          <Logout setUser={setUser}/>
+          {/* <button onClick={handleAdd}>new recipe</button> */}
+          <Link to={"/add-new"}>Add Recipe</Link>
+          <h1>Home </h1>
+          <ul>
+            <Route exact path={`/home/:recipeId`}>
+              <RecipeEdit recipe={recipe} setRecipe={setRecipe} />
+            </Route>
+            {recipes}
+          </ul>
+        </div>
+      {/* </Route> */}
+    </>
   );
 }
