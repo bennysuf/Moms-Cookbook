0808d0791a5e90769a934fef4bd2675383707319
Benny Sufrin <bennysuf@gmail.com>
2023-04-18 13:42:24 -0400

    adds useContext, logic for recipe categories, and simplifies fetch syntax

--- client/src/components/RecipeEdit.js
+++ client/src/components/RecipeEdit.js
@@ -1,51 +1,47 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
+import { UserContext } from "./App";
 
-export default function RecipeEdit({ recipe, setRecipe }) {
+export default function RecipeEdit() {
   const { recipeId } = useParams();
+
   const history = useHistory();
 
-  const recipes = recipe.filter((rec) => rec.id == recipeId); //=== doesn't work
-  const { title, directions, ingredients } = recipes[0];
+  const { recipe, setRecipe } = useContext(UserContext);
 
+  const recipes = recipe.filter((rec) => rec.id == recipeId); //=== doesn't work
+  const { title, directions, ingredients, categories } = recipes[0];
+
+  //useStates
   const [newTitle, setNewTitle] = useState(title);
   const [newDirections, setNewDirections] = useState(directions);
   const [newIngredients, setNewIngredients] = useState(ingredients);
+  const [newCategory, setNewCategory] = useState(categories[0].meal);
   const [errors, setErrors] = useState([]);
 
+  console.log(newCategory);
   function handleSub(e) {
     e.preventDefault();
     setErrors([]);
+    const newItem = {
+      id: parseInt(recipeId),
+      title: newTitle,
+      directions: newDirections,
+      ingredients: newIngredients,
+      category: newCategory,
+    };
+
     fetch(`/recipes/${recipeId}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        id: recipeId,
-        title: newTitle,
-        directions: newDirections,
-        ingredients: newIngredients,
-      }),
+      body: JSON.stringify(newItem),
     }).then((r) => {
       if (r.ok) {
-        r.json().then((d) => {
-          const update = recipe.map((item) => {
-              // const returns = recipe.filter(rec => {
-              //   const check = rec.id !== d.id && item
-              //   return check
-              // })
-              // console.log("Updated", returns)
-            if (item.id === d.id) {
-              return {
-                id: parseInt(recipeId),
-                title: newTitle,
-                directions: newDirections,
-                ingredients: newIngredients,
-              };
-            } else {
-              return item;
-            }
+        r.json().then((item) => {
+          const returns = recipe.map((rec) => {
+            return rec.id !== item.id ? rec : item;
           });
-          setRecipe(update);
+          setRecipe(returns);
           history.push("/home");
         });
       } else {
@@ -62,14 +58,16 @@ export default function RecipeEdit({ recipe, setRecipe }) {
 
   function onDelete() {
     fetch(`/recipes/${recipeId}`, {
-      method: "DELETE"
+      method: "DELETE",
     })
-      .then((r) => {r.json()}) //?need {} in order to prevent json error
+      .then((r) => {
+        r.json();
+      }) //?need {} in order to prevent json error
       .then(() => {
         setRecipe(recipe.filter((rec) => rec.id !== parseInt(recipeId)));
         history.push("/home");
       })
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -95,6 +93,13 @@ export default function RecipeEdit({ recipe, setRecipe }) {
         value={newIngredients}
         onChange={(e) => setNewIngredients(e.target.value)}
       />
+      <br />
+      <select onChange={(e) => setNewCategory(e.target.value)}>
+        <option value={newCategory}>Select meal</option>
+        <option value="Breakfast">Breakfast</option>
+        <option value="Lunch">Lunch</option>
+        <option value="Dinner">Dinner</option>
+      </select>
       <br />
       <button type="submit">submit</button>
       <button onClick={onDelete}>Delete recipe</button>
