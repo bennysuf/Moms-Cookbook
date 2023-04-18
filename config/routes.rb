fe8f7dad76a013a871efdc1124f6ec32693b17d8
Benny Sufrin <bennysuf@gmail.com>
2023-04-18 13:50:50 -0400

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # resources :recipe_categories
   resources :categories, only: [:index]
-  resources :recipes, only: [:create, :update, :destroy, :index]
-  resources :users, only: [:create, :show]
+  resources :recipe_categories
+  # resources :recipes, only: [:create, :update, :destroy, :index, :show]
+  resources :recipes
+  resources :users, only: [:index, :show]
+  get "/user", to: "users#show"
+  post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
