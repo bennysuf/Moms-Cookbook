a867a8c8c0617790f175e2081c11816bf4c39c23
Benny Sufrin <bennysuf@gmail.com>
2023-04-18 13:53:08 -0400

    checks if categories exist and add them if not

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,13 @@
 class CategoriesController < ApplicationController
     def index
+        if !Category.find_by(meal: "Breakfast")
+            Category.create(meal: "Breakfast")
+            Category.create(meal: "Lunch")
+            Category.create(meal: "Dinner")
+        end
         category = Category.all
         render json: category, status: :ok
     end
 end
+
+# TODO: how to start with categories
