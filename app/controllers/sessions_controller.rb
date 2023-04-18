a2c1ed9d8439063b2a45336c670cda699845e4c9
Benny Sufrin <bennysuf@gmail.com>
2023-04-18 14:01:10 -0400

    adds json logic to return proper error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-        if user&.authenticate(params[:password])
+        if !user
+            render json: { error: "Username non existant" }, status: :unauthorized
+        elsif !user&.authenticate(params[:password])
+            render json: { error: "Invalid password" }, status: :unauthorized
+        elsif user&.authenticate(params[:password]) 
             session[:user_id] = user.id
             render json: user, status: :created
-        else
-            render json: { error: "Invalid username or password" }, status: :unauthorized
         end
     end
 
