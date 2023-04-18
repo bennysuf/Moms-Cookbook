c55c69ba450d8c73e416528fe4a095da5f4136ed
Benny Sufrin <bennysuf@gmail.com>
2023-04-18 13:46:46 -0400

    adds useContext and cleans up comments

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,46 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Route, Link, useHistory } from "react-router-dom";
 import Logout from "./Logout";
-// import NewRecipe from "./NewRecipe";
 import RecipeCard from "./RecipeCard";
 import RecipeEdit from "./RecipeEdit";
+import { UserContext } from "./App";
 
-export default function Home({ user, setUser, setRecipe, recipe }) {
-// export default function Home({ user, setUser }) {
+export default function Home() {
+  const [recipes, setRecipes] = useState([]); //so page renders recipes when going to "/home"
+
+  const { user, recipe } = useContext(UserContext);
+
   const history = useHistory();
 
   if (!user) {
     history.push("/login");
   } //need this here, wont work in App component (will always push to login before user gets updated)
 
-//   const [recipe, setRecipe] = useState(user.recipes);
-
-  const recipes = recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec} />);
-
-  function handleAdd() {
-    //TODO: take me to new recipe route
-  }
+  useEffect(() => {
+    setRecipes(recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec} />));
+  }, [recipe]);
 
   return (
     <>
-      {/* <Route exact path="/add-new">
-        <NewRecipe recipe={recipe} setRecipe={setRecipe} />
-      </Route>  */}
-      {/* <Route path="home"> */}
-        <div>
-          {/* //   <div onClick={() => history.push("/home")}> */}
-          <Logout setUser={setUser}/>
-          {/* <button onClick={handleAdd}>new recipe</button> */}
-          <Link to={"/add-new"}>Add Recipe</Link>
-          <h1>Home </h1>
-          <ul>
-            <Route exact path={`/home/:recipeId`}>
-              <RecipeEdit recipe={recipe} setRecipe={setRecipe} />
-            </Route>
-            {recipes}
-          </ul>
-        </div>
-      {/* </Route> */}
+      <div style={{ textAlign: "center" }}>
+        <Logout />
+        <Link to={"/add-new"} style={{ margin: "1%" }}>
+          Add Recipe
+        </Link>
+        <ul>
+          <Route exact path={`/home/:recipeId`} component={RecipeEdit} />
+          {recipes}
+        </ul>
+      </div>
     </>
   );
 }
+
+//? edit page doesn't change, only when reloaded
