31aafb42f8bad90efb99741ca0f517fc1223e099
Benny Sufrin <bennysuf@gmail.com>
2023-04-21 14:46:49 -0400

    adds imports and components

---
+++ client/src/components/RecipePage.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { Route } from "react-router-dom";
+import RecipeCard from "./RecipeCard";
+import RecipeEdit from "./RecipeEdit";
+import NavBar from "./NavBar";
+import { UserContext } from "./App";
+
+export default function RecipePage() {
+  const { view } = useContext(UserContext);
+
+  return (
+    <>
+      <NavBar />
+      <Route exact path={`/recipes/:recipeId`} component={RecipeEdit} />
+      <RecipeCard key={view.id} recipe={view} />
+    </>
+  );
+}
