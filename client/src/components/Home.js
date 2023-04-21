d1e98c6fabb0f57ad49c6ca385194fd1cd9375f4
Benny Sufrin <bennysuf@gmail.com>
2023-04-21 14:54:26 -0400

    changes how recipe is mapped. cleans up return and adds NavBar. Adds view button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,39 +1,48 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Route, Link, useHistory } from "react-router-dom";
 import Logout from "./Logout";
-import RecipeCard from "./RecipeCard";
-import RecipeEdit from "./RecipeEdit";
+import NavBar from "./NavBar";
 import { UserContext } from "./App";
 
 export default function Home() {
   const [recipes, setRecipes] = useState([]); //so page renders recipes when going to "/home"
 
-  const { user, recipe } = useContext(UserContext);
+  const { user, recipe, setView } = useContext(UserContext);
 
   const history = useHistory();
 
-  if (!user) {
-    history.push("/login");
-  } //need this here, wont work in App component (will always push to login before user gets updated)
+  function handleClick(event) {
+    setView(event);
+    history.push("/recipes");
+    //? how to take user to RecipePage with just single recipe
+  }
+  // if (!user) {
+  // history.push("/login");
+  // } //need this here, wont work in App component (will always push to login before user gets updated)
+
+  const mapped = recipe.map((rec) => {
+    const { id, title, categories } = rec;
+    console.log(categories);
+    return (
+      <li key={id}>
+        <p>{title}</p>
+        {/* <p>{categories === [] ? null: categories[0]?.meal}</p> */}
+        <p>{categories[0]?.meal}</p>
+        <button onClick={() => handleClick(rec)}>View</button>
+      </li>
+    );
+  });
 
   useEffect(() => {
-    setRecipes(recipe.map((rec) => <RecipeCard key={rec.id} recipe={rec} />));
+    setRecipes(mapped);
   }, [recipe]);
 
   return (
     <>
       <div style={{ textAlign: "center" }}>
-        <Logout />
-        <Link to={"/add-new"} style={{ margin: "1%" }}>
-          Add Recipe
-        </Link>
-        <ul>
-          <Route exact path={`/home/:recipeId`} component={RecipeEdit} />
-          {recipes}
-        </ul>
+        <NavBar />
+        <ul>{recipes}</ul>
       </div>
     </>
   );
 }
-
-//? edit page doesn't change, only when reloaded
