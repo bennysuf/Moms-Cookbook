44d5cc165a4276a366a696b2316e4c532911defd
Benny Sufrin <bennysuf@gmail.com>
2023-04-21 15:01:23 -0400

    removes all unused crud

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,3 @@
 class CategoriesController < ApplicationController
-    def index
-        if !Category.find_by(meal: "Breakfast")
-            Category.create(meal: "Breakfast")
-            Category.create(meal: "Lunch")
-            Category.create(meal: "Dinner")
-        end
-        category = Category.all
-        render json: category, status: :ok
-    end
-end
 
-# TODO: how to start with categories
+end
