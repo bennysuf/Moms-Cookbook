dc3d717046621cb934c3ccf59233e8be44a90313
Benny Sufrin <bennysuf@gmail.com>
2023-04-21 15:00:33 -0400

    moves AR rescue and methods into file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    private
+
+    def find_user 
+        User.find_by(id: session[:user_id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Recipe not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+
 end
