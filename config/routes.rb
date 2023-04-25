bb40408cf507f9bbfe80daf2a105edd9de3b0c21
Benny Sufrin <bennysuf@gmail.com>
2023-04-25 13:58:40 -0400

    cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  # resources :recipe_categories
-  resources :categories, only: [:index]
   resources :recipe_categories
-  # resources :recipes, only: [:create, :update, :destroy, :index, :show]
-  resources :recipes
   resources :users, only: [:index, :show]
+  resources :recipes
   get "/user", to: "users#show"
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
