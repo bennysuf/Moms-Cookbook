95dc3e809b96b9aeee49ad055f221435ae59068c
Benny Sufrin <bennysuf@gmail.com>
2023-04-25 13:49:09 -0400

    changes syntax

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,24 @@
-import { NavLink } from "react-router-dom";
 import Logout from "./Logout";
 
 export default function NavBar() {
   return (
-    <div style={{ textAlign: "center" }}>
-      <NavLink style={{ marginRight: "10px" }} to="/home">
-        Home
-      </NavLink>
-      <NavLink style={{ marginRight: "10px" }} to="/add-new">
-        New recipe
-      </NavLink>
-      <Logout style={{ margin: "center" }}/>
-    </div>
+    <nav>
+      <ul style={{ marginLeft: "5%" }}>
+        <li>
+          <strong>Mom's Cookbook</strong>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/add-new">New recipe</a>
+        </li>
+        <li>
+          <Logout />
+        </li>
+      </ul>
+    </nav>
   );
 }
