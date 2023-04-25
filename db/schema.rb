ec324add3b86d59c20e9f8a54672b0b78bf4cd4e
Benny Sufrin <bennysuf@gmail.com>
2023-04-25 13:59:20 -0400

    adds row to categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_184253) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_204900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_184253) do
     t.string "meal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "recipe_categories", force: :cascade do |t|
