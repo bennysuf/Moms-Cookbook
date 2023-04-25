7e1eb902dae12493f1f0e85efc13948c734df4be
Benny Sufrin <bennysuf@gmail.com>
2023-04-25 14:04:29 -0400

    updates imports and contex. adds ternary to setRecipes. cleans up comments

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,40 +1,36 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Route, Link, useHistory } from "react-router-dom";
-import Logout from "./Logout";
+import { useHistory } from "react-router-dom";
 import NavBar from "./NavBar";
 import { UserContext } from "./App";
 
 export default function Home() {
-  const [recipes, setRecipes] = useState([]); //so page renders recipes when going to "/home"
+  const [recipes, setRecipes] = useState([]);
 
-  const { user, recipe, setView } = useContext(UserContext);
+  const { recipe, setView } = useContext(UserContext);
 
   const history = useHistory();
 
   function handleClick(event) {
     setView(event);
     history.push("/recipes");
-    //? how to take user to RecipePage with just single recipe
   }
-  // if (!user) {
-  // history.push("/login");
-  // } //need this here, wont work in App component (will always push to login before user gets updated)
 
   const mapped = recipe.map((rec) => {
     const { id, title, categories } = rec;
-    console.log(categories);
     return (
-      <li key={id}>
+      <article key={id}>
         <p>{title}</p>
-        {/* <p>{categories === [] ? null: categories[0]?.meal}</p> */}
-        <p>{categories[0]?.meal}</p>
-        <button onClick={() => handleClick(rec)}>View</button>
-      </li>
+        {/* <p>{categories[0].meal}</p> //? why does categories only show after reload? */}
+        {/* <p>{categories ? categories[0]?.meal : "Loading..."}</p> //TODO: need page to reload / renders categories once and gets loading */}
+        <button className="button" onClick={() => handleClick(rec)}>
+          View
+        </button>
+      </article>
     );
   });
 
   useEffect(() => {
-    setRecipes(mapped);
+    setRecipes(mapped[0] ? mapped : "Such emptiness");
   }, [recipe]);
 
   return (
