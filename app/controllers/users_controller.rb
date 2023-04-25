6633ca72a1c6c24f383b26dc873babf099295b16
Benny Sufrin <bennysuf@gmail.com>
2023-04-25 13:56:50 -0400

    moves rescue and method out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def create
         user = User.create(user_params)
@@ -13,7 +11,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find_by(id: session[:user_id])
+        user = find_user
         if user
             render json: user, include: ['recipes', 'recipes.categories'], status: :ok
         else
@@ -22,7 +20,7 @@ class UsersController < ApplicationController
     end
 
     def index
-        user = User.find_by(id: session[:user_id])
+        user = find_user
         if user
             user = User.all
             render json: user, include: ['recipes', 'recipes.categories'], status: :ok
@@ -35,13 +33,5 @@ class UsersController < ApplicationController
 
     def user_params
         params.permit(:username, :password, :password_confirmation)
-    end
-
-    def render_not_found_response
-        render json: { error: "User not found" }, status: :not_found
-    end
-
-    def render_unprocessable_entity_response(invalid)
-        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
     end
 end
