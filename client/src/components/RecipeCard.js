dd3ace2c54b7a6e5871bbc6d662f27d1cc2d4dcc
Benny Sufrin <bennysuf@gmail.com>
2023-04-25 13:47:55 -0400

    adds styling

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -4,12 +4,12 @@ export default function RecipeCard({ recipe }) {
   const { id, title, directions, ingredients, categories } = recipe;
 
   return (
-    <div>
-        <h4>{title}</h4>
-        <h5>Meal: {categories[0]?.meal}</h5>
-        <p>Directions: {directions}</p>
-        <p>Ingredients: {ingredients}</p>
-        <Link to={`/recipes/${id}`}>Edit</Link>
+    <div style={{ textAlign: "center" }}>
+      <h4>{title}</h4>
+      <h5>Meal: {categories ? categories[0]?.meal : "Loading..."}</h5>
+      <p>Directions: {directions}</p>
+      <p>Ingredients: {ingredients}</p>
+      <Link to={`/recipes/${id}`}>Edit</Link>
     </div>
   );
 }
