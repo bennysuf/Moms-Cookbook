a9ecbac8e30976ae4f43e97eb589c2f2d75a7fdc
Benny Sufrin <bennysuf@gmail.com>
2023-04-26 14:21:16 -0400

    makes code DRY

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,47 +2,37 @@ class RecipesController < ApplicationController
 
     def create
         user = find_user
-        if user 
-            category = Category.create!(
-                meal: params[:category],
-                user_id: user.id
-            )
-                recipe = user.recipes.create!(recipe_params)
-                joiner = RecipeCategory.create!(recipe_id: recipe.id, category_id: category.id)
-                render json: recipe, status: :created
-        end
+        category = Category.create!(
+            meal: params[:category],
+            user_id: user.id
+        )
+        recipe = user.recipes.create!(recipe_params)
+        joiner = RecipeCategory.create!(recipe_id: recipe.id, category_id: category.id)
+        render json: recipe, status: :created
     end
 
     def update
         user = find_user
-        if user
         recipe = Recipe.find_by_id(params[:id])
         category = recipe.categories.first
-            joiner = RecipeCategory.find_by(recipe_id: params[:id], category_id: category.id) 
-            recipe.update!(recipe_params)
-            if params[:category] == "Breakfast" || params[:category] == "Lunch" || params[:category] == "Dinner"
-                category.update!(meal: params[:category])
-                joiner.update!(category_id: category.id)
-            end
-            render json: recipe, status: :accepted
+        joiner = RecipeCategory.find_by(recipe_id: params[:id], category_id: category.id) 
+        recipe.update!(recipe_params)
+        if params[:category] == "Breakfast" || params[:category] == "Lunch" || params[:category] == "Dinner"
+            category.update!(meal: params[:category])
+            joiner.update!(category_id: category.id)
         end
+        render json: recipe, status: :accepted
     end
 
     def destroy
-        user = find_user
-        if user
-            recipe = Recipe.find_by_id(params[:id]) #?unxepeted Unexpected end of JSON input
-            recipe.destroy
-            render json: {}
-        end
+        recipe = Recipe.find_by_id(params[:id])
+        recipe.destroy
+        render json: {}
     end
 
     def show
-        user = find_user
-        if user
-            recipe = Recipe.find_by_id(params[:id])
-            render json: recipe, status: :ok
-        end
+        recipe = Recipe.find_by_id(params[:id])
+        render json: recipe, status: :ok
     end
 
     def index 
