f635bb1b3e108328c273657a9fc3b1a66670daef
Benny Sufrin <bennysuf@gmail.com>
2023-04-27 13:11:36 -0400

    removes unused attribute

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :directions, :ingredients, :categories
+  attributes :id, :title, :directions, :ingredients
   
   has_one :user
 end
