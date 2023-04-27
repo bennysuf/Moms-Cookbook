3d58ab8ddfe6a302f6d424ddfc591841bb72fba0
Benny Sufrin <bennysuf@gmail.com>
2023-04-27 13:16:15 -0400

    adds destructoring and return for new data

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 
-export default function RecipeCard({ recipe }) {
-  const { id, title, directions, ingredients, categories } = recipe;
+export default function RecipeCard({ recipes }) {
+  const {difficulty, id, recipe, category} = recipes
+  const {title, directions, ingredients} = recipe
+  const {meal} = category
 
   return (
     <div style={{ textAlign: "center" }}>
       <h4>{title}</h4>
-      <h5>Meal: {categories ? categories[0]?.meal : "Loading..."}</h5>
+      <h5>Meal: {meal}</h5>
+      <em>Difficulty: {difficulty}</em>
       <p>Directions: {directions}</p>
       <p>Ingredients: {ingredients}</p>
       <Link to={`/recipes/${id}`}>Edit</Link>
