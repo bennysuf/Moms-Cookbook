853a1e0a14de99e9546d4e5272cd7d6af4f31556
Benny Sufrin <bennysuf@gmail.com>
2023-04-27 13:10:34 -0400

    changes relationship

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_categories, dependent: :destroy
-  has_many :categories, through: :recipe_categories, dependent: :destroy
+  has_many :categories, through: :recipe_categories
 
   validates :title, presence: true
   validates :directions, presence: true
