bd4b28f11f2fff444b4cd24003d25a70c70d8967
Benny Sufrin <bennysuf@gmail.com>
2023-04-28 13:46:30 -0400

    adds CRUD and removes controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,23 @@
 class CategoriesController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+    def create
+        check = Category.find_by(category: params[:category])
+        if check == nil
+            category = Category.create!(category_params)
+            render json: category, status: :created
+        else 
+            render json: {error: "Category exists already"}, status: :unprocessable_entity
+        end
+    end
 
+    def index
+        category = Category.all.order("created_at DESC")
+        render json: category, status: :ok
+    end
+
+    private 
+
+    def category_params
+        params.permit(:category)
+    end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,53 @@
 class RecipesController < ApplicationController
 
+    def create
+        user = find_user
+        category = Category.find_by(category: params[:category])
+        if category == nil 
+            render json: {error: "Select a category"}, status: :unprocessable_entity
+        else
+            recipe = user.recipes.create!(
+                title: params[:title], 
+                directions: params[:directions],
+                ingredients: params[:ingredients],
+                category_id: category.id
+                )
+            render json: recipe, status: :created
+        end
+    end
+
+    def update
+        category = Category.find_by(category: params[:category])
+        recipe = Recipe.find_by_id(params[:id])
+        recipe.update!(
+            title: params[:title], 
+            directions: params[:directions],
+            ingredients: params[:ingredients],
+            category_id: category.id
+            )
+        render json: recipe, status: :accepted 
+    end
+
+    def destroy
+        recipe = Recipe.find_by_id(params[:id])
+        recipe.destroy
+        render json: {}
+    end
+
+    def show
+        user = find_user
+        recipe = user.recipes.all
+        render json: recipe, status: :ok
+    end
+
+    def index 
+        recipe = Recipe.all.order("created_at DESC")
+        render json: recipe, status: :ok
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:id, :title, :directions, :ingredients, :category)
+    end
 end
