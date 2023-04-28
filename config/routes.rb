82eec99f4d86ba5dd726cda82a0b296f8187cc81
Benny Sufrin <bennysuf@gmail.com>
2023-04-28 13:48:31 -0400

    changes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :recipe_categories
+  # resources :recipe_categories
   resources :users, only: [:index, :show]
   resources :recipes
+  resources :categories, only: [:create, :index, :show]
   get "/user", to: "users#show"
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
